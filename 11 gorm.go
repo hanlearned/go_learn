9755584d1e23552945f5df72a37acd3dacb0e06d
hanlearned <hanlearned99@gmail.com>
2023-10-17 17:33:02 +0800

    feat: add form function

--- 11 gorm.go
+++ 11 gorm.go
@@ -22,8 +22,39 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	//stu := &Students{6, "han123", "男", 19, "class A"}
+	// 插入一条数据
+	//stu := &Students{7, "han234", "男", 20, "class B"}
 	//db.Create(&stu)
+	//db.Commit()
 
-	db.Commit()
+	// 获取所有数据
+	var allStudents []Students
+	db.Find(&allStudents) // select * from students
+	fmt.Println(allStudents)
+
+	// 获取第一条数据
+	var firstStudent Students
+	db.First(&firstStudent) // select * from students order by student_id limit 1
+	fmt.Println(firstStudent)
+
+	// 获取最后一条数据
+	var lastStudent Students
+	db.Last(&lastStudent) // select * from students order by student_id desc limit 1
+	fmt.Println(lastStudent)
+
+	// 根据主键查询, 获取单条数据
+	var singleStudent Students
+	db.Take(&singleStudent, 2) // select * from students where student_id = 2
+	fmt.Println(singleStudent)
+
+	// 按条件查询
+	var queryStudent []Students
+	db.Find(&queryStudent, "class = ?", "Class A") // select * from students where class = "Class A"
+	fmt.Println(queryStudent)
+
+	// where 条件查询
+	var whereStudent []Students
+	db.Where("name = ?", "Alice").Find(&whereStudent)
+	fmt.Println(whereStudent)
+
 }
